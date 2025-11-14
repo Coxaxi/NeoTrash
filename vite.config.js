9c489f15e3d9158b5950ebd2cbb6546e34f97e10
Coxaxi <202415210059@ifba.edu.br>
2025-11-14 17:25:08 -0300

    Fix2: Corrigido caminhos de imagem para deploy na Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ← MUDAR PARA RAIZ
+  base: '/', // Ou tente remover completamente esta linha
 });
