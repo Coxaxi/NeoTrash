e41d64685d72b035aa61511f84a9e3f52fff75f4
Coxaxi <202415210059@ifba.edu.br>
2025-11-14 16:55:19 -0300

    Fix: Corrigido pasta raiz para deploy na vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NeoTrash/',  // Substitua por '/nome-do-seu-repo/' (com a barra no final). Se for o repo root, use '/'.
+  base: '/',  // ← MUDAR PARA RAIZ
 });
